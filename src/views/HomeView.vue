8d96f4d431b601262a8031719e7a3db8bc39acd0
unknown <silasudofia469@gmail.com>
2022-12-26 16:10:34 -0800

    updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -198,25 +198,108 @@ sm="4"
   </v-card>
       </v-col>
 
+
+
+
+
+
+
+
+
+
       <v-col
         cols="12"
         sm="7"
       >
-      <v-card
-    class="mx-auto"
-    max-width="100vw"
-      >
-    
-    <v-list
-      :items="items"
-      item-props
-      lines="three"
+      <v-expansion-panels variant="popout" class="pa-4 d-none d-sm-flex">
+    <v-expansion-panel
+      v-for="(message, i) in messages"
+      :key="i"
+      hide-actions
     >
-      <template v-slot:subtitle="{ subtitle }">
-        <div v-html="subtitle"></div>
-      </template>
-    </v-list>
-  </v-card>
+      <v-expansion-panel-title>
+        <v-row
+          align="center"
+          class="spacer"
+          no-gutters
+        >
+          <v-col
+            cols="4"
+            sm="2"
+            md="1"
+          >
+            <v-avatar
+              size="36px"
+            >
+              <v-img
+                v-if="message.avatar"
+                alt="Avatar"
+                src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
+              ></v-img>
+              <v-icon
+                v-else
+                :color="message.color"
+                :icon="message.icon"
+              ></v-icon>
+            </v-avatar>
+          </v-col>
+
+
+          
+
+          <v-col
+            class="hidden-xs-only text-left ml-2"
+            sm="5"
+            md="3"
+          >
+            <strong v-html="message.name"></strong>
+            <span
+              v-if="message.total"
+              class="text-grey"
+            >
+              &nbsp;({{ message.total }})
+            </span>
+          </v-col>
+
+          <v-col
+            class="text-no-wrap text-left"
+            cols="5"
+            sm="3"
+          >
+            <v-chip
+              v-if="message.new"
+              :color="`${message.color}-lighten-1`"
+              class="ml-0 mr-2"
+              label
+              small
+            >
+              {{ message.new }} new
+            </v-chip>
+            <strong v-html="message.title"></strong>
+          </v-col>
+
+
+          
+
+          <v-col
+            v-if="message.excerpt"
+            class="text-medium-emphasis text-truncate hidden-sm-and-down"
+          >
+            &mdash;
+            {{ message.excerpt }}
+          </v-col>
+        </v-row>
+      </v-expansion-panel-title>
+
+      <v-expansion-panel-text>
+        <v-card-text v-text="lorem"></v-card-text>
+      </v-expansion-panel-text>
+
+      <v-expansion-panel-text>
+        <v-card-text v-text="lorem"></v-card-text>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
       </v-col>
     </v-row>
   </v-container>
@@ -315,11 +398,12 @@ sm="4"
         
       </v-col>
 
+
+
       <v-col
         cols="12"
         sm="4"
       >
-
 
 <v-card
     :loading="true"
@@ -560,9 +644,37 @@ export default {
           title: 'Recipe to try',
           subtitle: '<span class="text-primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
         },
-        ])       
+        ])    
+        
+    const messages = ref( [
+        {
+          avatar: 'https://avatars0.githubusercontent.com/u/9064066?v=4&s=460',
+          name: 'John Leider',
+          title: 'Welcome to Vuetify!',
+          excerpt: 'Thank you for joining our community...',
+        },
+        {
+          color: 'red',
+          icon: 'mdi-account-multiple',
+          name: 'Social',
+          new: 1,
+          total: 3,
+          title: 'Twitter',
+        },
+        {
+          color: 'teal',
+          icon: 'mdi-tag',
+          name: 'Promos',
+          new: 2,
+          total: 4,
+          title: 'Shop your way',
+          exceprt: 'New deals available, Join Today',
+        },
+      ],)   
+      
+      const lorem = ref('Lorem ipsum dolor sit amet, at aliquam vivendum vel, everti delicatissimi cu eos. Dico iuvaret debitis mel an, et cum zril menandri. Eum in consul legimus accusam. Ea dico abhorreant duo, quo illum minimum incorrupte no, nostro voluptaria sea eu. Suas eligendi ius at, at nemore equidem est. Sed in error hendrerit, in consul constituam cum.')
 
-  return {colors, slides, items}      
+  return {colors, slides, items, messages, lorem}      
    
   },
   components:{
