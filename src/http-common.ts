80ef69a195c5a484cafb10c987473bc9ee025b53
unknown <silasudofia469@gmail.com>
2023-03-11 00:32:36 -0800

    updated

--- src/http-common.ts
+++ src/http-common.ts
@@ -4,6 +4,8 @@ const apiClient = axios.create({
   baseURL: "http://sundaymba2-001-site1.atempurl.com/api",
   headers: {
     "Content-type": "application/json",
+    "Accept": "application/json",
+    "Access-Control-Allow-Origin": "*"
   },
 });
 
